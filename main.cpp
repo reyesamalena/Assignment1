6f78e637c8f334df240558124c7f1878af0184e5
Malena <reyesamalena@gmail.com>
2019-10-21 17:08:11 -0500

    RadixSort

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include "quicksort.h"
 #include <iostream>
-
+#include "radixsort.h"
 
 using namespace std;
 
-// Driver Code 
 int main()  
-{  
+{
+    
+    //quick sort
     int arr[] = {10, 7, 8, 9, 1, 5};  
     int n = sizeof(arr) / sizeof(arr[0]);  
     quickSort(arr, 0, n - 1);  
-    cout << "Sorted array: \n";  
-    printArray(arr, n);  
+    cout << "Quicksort Sorted Array: \n";
+    printArray(arr, n);
+    
+    //radix sort
+    int Radixarr[] = {170, 45, 75, 90, 802, 24, 2, 66};
+    int Rn = sizeof(Radixarr)/sizeof(Radixarr[0]);
+    cout << "Radixsort Sorted Array: \n"; 
+    radixsort(Radixarr, Rn);
+    print(Radixarr, Rn);
+    
     return 0;  
 }  
---
+++ radixsort.h
@@ -0,0 +1,22 @@
+#ifndef radixsort_H
+#define radixsort_H
+#include<iostream>
+
+using namespace std;
+  
+// A utility function to get maximum value in arr[]
+int getMax(int arr[], int n);
+  
+// A function to do counting sort of arr[] according to
+// the digit represented by exp.
+void countSort(int arr[], int n, int exp);
+  
+// The main function to that sorts arr[] of size n using
+// Radix Sort
+void radixsort(int arr[], int n);
+  
+// A utility function to print an array
+void print(int arr[], int n);
+
+#endif
+  
