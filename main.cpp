daaef10a0552d0a6f7eff54ad28df66ff99e8c5b
zcb0015 <zcb0015@unt.edu>
2019-10-27 17:08:42 -0600

    added printing and storing times for all arrays

--- main.cpp
+++ main.cpp
@@ -36,28 +36,42 @@ int main()
     int array_three[N] = {0};//Sets where no numbers repeat and each integer has 20 digits
     int randomposition;
     
-    //Arrays to keep test results
-    int quick_Sort[10];
-    int opt_Quick_Sort[10];
-    int radix_Sort[10];
-    int tim_Sort[10];
+    //Arrays to keep test results for Array 1
+    int quick_Sort1[10];
+    int opt_Quick_Sort1[10];
+    int radix_Sort1[10];
+    int tim_Sort1[10];
     
+    //Arrays to keep test results for Array 2
+    int quick_Sort2[10];
+    int opt_Quick_Sort2[10];
+    int radix_Sort2[10];
+    int tim_Sort2[10];
+   
+    //Arrays to keep test results for Array 3
+    int quick_Sort3[10];
+    int opt_Quick_Sort3[10];
+    int radix_Sort3[10];
+    int tim_Sort3[10];
+
     for( int j= 0; j<10; j++){
         
-	int trial_num = j+1;	//keep track of trial number w/o messing with counter
+//	int trial_num = j+1;	//keep track of trial number w/o messing with counter
 
         cout << "\033[1;31m";       //printing color
-        cout  << "Trial " << trial_num <<": "<< endl;       //trial number
+        cout  << "Trial " << j <<": "<< endl;       //trial number
         cout << "\033[0m";
         
+cout << j << endl;
         for(int k = 0; k < (million_start+M); k++){ //for array one
             array_one[k-million_start] = k;
             
-        }
-        
+        } 
+cout << j << endl;
         random_shuffle(std::begin(array_one), std::end(array_one)); //randomly shuffling the array one
 
         
+cout << j << endl;
         for( unsigned long long int n = one_percent_start; n< (one_percent_start+N); n++){ //for array two
             array_two[n-one_percent_start] = n;
             array_two[ (n-one_percent_start) +1] = n;
@@ -71,46 +85,122 @@ int main()
             array_three[i-twentydigit_start] = i;
         }
         random_shuffle(array_three, array_three +N); //randomly shuffling the array
-        
+       
+
 	//QUICK SORT
      
-         auto quick_start = high_resolution_clock::now();
-         quickSort(array_three, 0, N - 1);
-         auto quick_stop = high_resolution_clock::now();
-         auto quick_duration = duration_cast<microseconds>(quick_stop - quick_start);
+	 //Array 1
+         auto quick_start1 = high_resolution_clock::now();
+         quickSort(array_one, 0, N - 1);
+         auto quick_stop1 = high_resolution_clock::now();
+         auto quick_duration1 = duration_cast<microseconds>(quick_stop1 - quick_start1);
          
          cout << "Quicksort Sorted Array: \n";
          //printArray(arr, n);
-         cout << "Time: " << quick_duration.count() << " microsecond " << endl;
+         cout << "Array 1 Time: " << quick_duration1.count() << " microsecond " << endl;
          cout << endl;
-        quick_Sort[j]=quick_duration.count();
+         quick_Sort1[j]=quick_duration1.count();
          
+	 //Array 2
+         auto quick_start2 = high_resolution_clock::now();
+         quickSort(array_two, 0, N - 1);
+         auto quick_stop2 = high_resolution_clock::now();
+         auto quick_duration2 = duration_cast<microseconds>(quick_stop2 - quick_start2);
+         
+         cout << "Quicksort Sorted Array: \n";
+         //printArray(arr, n);
+         cout << "Array 2 Time: " << quick_duration2.count() << " microsecond " << endl;
+         cout << endl;
+         quick_Sort2[j]=quick_duration2.count();
+
+	 //Array 3
+         auto quick_start3 = high_resolution_clock::now();
+         quickSort(array_three, 0, N - 1);
+         auto quick_stop3 = high_resolution_clock::now();
+         auto quick_duration3 = duration_cast<microseconds>(quick_stop3 - quick_start3);
+         
+         cout << "Quicksort Sorted Array: \n";
+         //printArray(arr, n);
+         cout << "Array 3 Time: " << quick_duration3.count() << " microsecond " << endl;
+         cout << endl;
+         quick_Sort3[j]=quick_duration3.count();
+
+
     //OPTIMIZED QUICK SORT
 
-         auto o_quick_start = high_resolution_clock::now();
-         optimizedQuickSort(array_three, 0, N-1);
-         auto o_quick_stop = high_resolution_clock::now();
-         auto o_quick_duration = duration_cast<microseconds>(o_quick_stop - o_quick_start);
+	 //Array 1
+         auto o_quick_start1 = high_resolution_clock::now();
+         optimizedQuickSort(array_one, 0, N-1);
+         auto o_quick_stop1 = high_resolution_clock::now();
+         auto o_quick_duration1 = duration_cast<microseconds>(o_quick_stop1 - o_quick_start1);
         
          printf("Optimized Quick Sorted Array: \n");
          //printOQarray(OQuickarr, oqn);
-         cout << "Time: " << o_quick_duration.count() << " microsecond " << endl;
+         cout << "Array 1 Time: " << o_quick_duration1.count() << " microsecond " << endl;
          cout << endl;
-         opt_Quick_Sort[j]= o_quick_duration.count();
+         opt_Quick_Sort1[j]= o_quick_duration1.count();
+
+	 //Array 2
+         auto o_quick_start2 = high_resolution_clock::now();
+         optimizedQuickSort(array_two, 0, N-1);
+         auto o_quick_stop2 = high_resolution_clock::now();
+         auto o_quick_duration2 = duration_cast<microseconds>(o_quick_stop2 - o_quick_start2);
+        
+         printf("Optimized Quick Sorted Array: \n");
+         //printOQarray(OQuickarr, oqn);
+         cout << "Array 2 Time: " << o_quick_duration2.count() << " microsecond " << endl;
+         cout << endl;
+         opt_Quick_Sort2[j]= o_quick_duration2.count();
+
+	 //Array 3
+         auto o_quick_start3 = high_resolution_clock::now();
+         optimizedQuickSort(array_three, 0, N-1);
+         auto o_quick_stop3 = high_resolution_clock::now();
+         auto o_quick_duration3 = duration_cast<microseconds>(o_quick_stop3 - o_quick_start3);
+        
+         printf("Optimized Quick Sorted Array: \n");
+         //printOQarray(OQuickarr, oqn);
+         cout << "Array 3 Time: " << o_quick_duration3.count() << " microsecond " << endl;
+         cout << endl;
+         opt_Quick_Sort3[j]= o_quick_duration3.count();
 
     //RADIX SORT
 
-         
-         auto radix_start = high_resolution_clock::now();
-         radixsort(array_three, N);
-         auto radix_stop = high_resolution_clock::now();
-         auto radix_duration = duration_cast<microseconds>(radix_stop - radix_start);
+         //Array 1 
+         auto radix_start1 = high_resolution_clock::now();
+         radixsort(array_one, N);
+         auto radix_stop1 = high_resolution_clock::now();
+         auto radix_duration1 = duration_cast<microseconds>(radix_stop1 - radix_start1);
          
          cout << "Radixsort Sorted Array: \n";
          //print(Radixarr, Rn);
-         cout << "Time: " << radix_duration.count() << " microsecond " << endl;
+         cout << "Array 1 Time: " << radix_duration1.count() << " microsecond " << endl;
          cout << endl;
-         radix_Sort[j] = radix_duration.count();
+         radix_Sort1[j] = radix_duration1.count();
+
+         //Array 2 
+         auto radix_start2 = high_resolution_clock::now();
+         radixsort(array_two, N);
+         auto radix_stop2 = high_resolution_clock::now();
+         auto radix_duration2 = duration_cast<microseconds>(radix_stop2 - radix_start2);
+         
+         cout << "Radixsort Sorted Array: \n";
+         //print(Radixarr, Rn);
+         cout << "Array 2 Time: " << radix_duration2.count() << " microsecond " << endl;
+         cout << endl;
+         radix_Sort2[j] = radix_duration2.count();
+
+         //Array 3 
+         auto radix_start3 = high_resolution_clock::now();
+         radixsort(array_three, N);
+         auto radix_stop3 = high_resolution_clock::now();
+         auto radix_duration3 = duration_cast<microseconds>(radix_stop3 - radix_start3);
+         
+         cout << "Radixsort Sorted Array: \n";
+         //print(Radixarr, Rn);
+         cout << "Array 3 Time: " << radix_duration3.count() << " microsecond " << endl;
+         cout << endl;
+         radix_Sort3[j] = radix_duration3.count();
 
     //TIM SORT
     /*     int Timarr[] = {5, 21, 7, 23, 19};
@@ -144,7 +234,10 @@ int main()
     const int nameWidth = 21;
     const int numWidth = 10;
     int x;
-    
+   
+
+//Array 1
+    cout << "\t\t\t\tArray 1" << endl;
     //Print out column names
     printElement("Algorithms", nameWidth);
     for(x=1;x<=10;x++) {
@@ -154,28 +247,96 @@ int main()
     //Print 1st row for QuickSort
     printElement("QuickSort", nameWidth);
     for(x=0;x<10;x++) {
-	    printElement(quick_Sort[x], numWidth); }
+	    printElement(quick_Sort1[x], numWidth); }
     cout << endl;
     
     //Print 2nd row for Optimized QuickSort
     printElement("Optimized QuickSort", nameWidth);
     for(x=0;x<10;x++) {
-	    printElement(opt_Quick_Sort[x], numWidth); }
+	    printElement(opt_Quick_Sort1[x], numWidth); }
     cout << endl;
 
     //Print 3rd row for RadixSort
     printElement("RadixSort", nameWidth);
     for(x=0;x<10;x++) {
-	    printElement(radix_Sort[x], numWidth); }
+	    printElement(radix_Sort1[x], numWidth); }
     cout << endl;
     
     //Print 4th row for TimSort
     printElement("TimSort", nameWidth);
     for(x=0;x<10;x++) {
-	   // printElement(tim_Sort[x], numWidth);
+	   // printElement(tim_Sort1[x], numWidth);
+        
+    }
+    cout << endl << endl;
+   
+//Array 2
+    cout << "\t\t\t\tArray 2" << endl;
+    //Print out column names
+    printElement("Algorithms", nameWidth);
+    for(x=1;x<=10;x++) {
+	    cout<< "Trial " << x << "   "; }
+    cout << endl;
+
+    //Print 1st row for QuickSort
+    printElement("QuickSort", nameWidth);
+    for(x=0;x<10;x++) {
+	    printElement(quick_Sort2[x], numWidth); }
+    cout << endl;
+    
+    //Print 2nd row for Optimized QuickSort
+    printElement("Optimized QuickSort", nameWidth);
+    for(x=0;x<10;x++) {
+	    printElement(opt_Quick_Sort2[x], numWidth); }
+    cout << endl;
+
+    //Print 3rd row for RadixSort
+    printElement("RadixSort", nameWidth);
+    for(x=0;x<10;x++) {
+	    printElement(radix_Sort2[x], numWidth); }
+    cout << endl;
+    
+    //Print 4th row for TimSort
+    printElement("TimSort", nameWidth);
+    for(x=0;x<10;x++) {
+	   // printElement(tim_Sort2[x], numWidth);
+        
+    }
+    cout << endl << endl;
+
+//Array 3
+    cout << "\t\t\t\tArray 3" << endl;
+    //Print out column names
+    printElement("Algorithms", nameWidth);
+    for(x=1;x<=10;x++) {
+	    cout<< "Trial " << x << "   "; }
+    cout << endl;
+
+    //Print 1st row for QuickSort
+    printElement("QuickSort", nameWidth);
+    for(x=0;x<10;x++) {
+	    printElement(quick_Sort3[x], numWidth); }
+    cout << endl;
+    
+    //Print 2nd row for Optimized QuickSort
+    printElement("Optimized QuickSort", nameWidth);
+    for(x=0;x<10;x++) {
+	    printElement(opt_Quick_Sort3[x], numWidth); }
+    cout << endl;
+
+    //Print 3rd row for RadixSort
+    printElement("RadixSort", nameWidth);
+    for(x=0;x<10;x++) {
+	    printElement(radix_Sort3[x], numWidth); }
+    cout << endl;
+    
+    //Print 4th row for TimSort
+    printElement("TimSort", nameWidth);
+    for(x=0;x<10;x++) {
+	   // printElement(tim_Sort3[x], numWidth);
         
     }
     cout << endl;
-    
+
     return 0;  
 }  
